9192bc5d5c45a904fe198c833c25bda72dc1c282
shinkeman <shinkeman@yandex-team.ru>
2024-11-17 13:30:49 +0300

    initial commit with gitignore

---
+++ demoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  demoApp
+//
+//  Created by Vladislav Shinkevich on 17.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
